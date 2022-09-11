56699642c4af3cc99fbb993043d6ac09292bf7f5
Jeanluca <jeanlucafp@gmail.com>
2022-09-11 19:58:35 -0300

    PDF and Testes OK

---
+++ src/gerar-pdf.js
@@ -0,0 +1,55 @@
+const PDFDocument = require('pdfkit')
+const fs = require('fs')
+const path = require('path')
+
+function gerar() {
+    const doc = new PDFDocument()
+    const filename = 'stefany.pdf'
+    const imagesDir = 'images_insta'
+
+    doc.pipe(fs.createWriteStream(filename))
+
+    const directoryPath = path.resolve(__dirname, '..', imagesDir)
+
+    fs.readdir(directoryPath, (err, files) => {
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        }
+
+        files.forEach(function (file) {
+            const instaUser = file.replace('.jpg', '')
+
+            doc.image(path.resolve(__dirname, '..', imagesDir, file), {
+                fit: [50, 50],
+                align: 'right',
+                valign: 'right'
+            })
+
+            doc.text(instaUser, {
+                width: 300,
+                align: 'center'
+            })
+
+        });
+
+        doc.end()
+
+        const deleteFolderRecursive = function (path) {
+            if (fs.existsSync(path)) {
+                fs.readdirSync(path).forEach(function (file, index) {
+                    var curPath = path + "/" + file;
+                    if (fs.lstatSync(curPath).isDirectory()) { // recurse
+                        deleteFolderRecursive(curPath);
+                    } else { // delete file
+                        fs.unlinkSync(curPath);
+                    }
+                });
+                fs.rmdirSync(path);
+            }
+        };
+
+        deleteFolderRecursive(directoryPath)
+    })
+}
+
+gerar()
---
+++ src/testes.js
@@ -0,0 +1,101 @@
+const Authenticator = require('./auth')
+const DirectMessaging = require('./direct-messaging')
+const fs = require('fs')
+const request = require('request')
+const path = require('path')
+
+const auth = new Authenticator()
+
+auth.login()
+  .then(async client => {
+    const currentUser = await client.account.currentUser()
+    console.log(`Logged in as ${currentUser.username}`)
+
+    const dm = new DirectMessaging(client)
+    const followers = await dm.listFollowers('colodel_stefany_')
+    console.log('get followers')
+
+    const follows = await dm.listFollowing('colodel_stefany_')
+    console.log('get follows')
+
+    evidenciarNomeEFotoQuemEuSigoENaoMeSegue(followers, follows)
+    //quemEuSigoENaoMeSegue(followers, follows)
+    //dm.init()
+    // dm.sendDm('jeanlucafp')
+  })
+  .catch(err => {
+    console.error(err)
+  })
+
+function quemMeSegueAndEuNaoSigoDeVolta(followers, follows) {
+  const userNameFollowers = followers.map(item => item.username)
+  const userNameFollows = follows.map(item => item.username)
+
+  userNameFollowers.forEach(user => {
+    const profile = userNameFollows.find(follow => follow === user)
+    if (!profile) {
+      console.log('voce nao segue de volta o ', user)
+    }
+  })
+}
+
+function quemEuSigoENaoMeSegue(followers, follows) {
+  const userNameFollowers = followers.map(item => item.username)
+  const userNameFollows = follows.map(item => item.username)
+
+  userNameFollows.forEach(user => {
+    const profile = userNameFollowers.find(follow => follow === user)
+    if (!profile) {
+      console.log('nao esta te seguindo de volta ', user)
+    }
+  })
+}
+
+function evidenciarNomeEFotoQuemEuSigoENaoMeSegue(followers, follows) {
+  const evidencias = []
+  const structFollowers = followers.map(item => ({
+    username: item.username,
+    profilePicUrl: item.profile_pic_url
+  }))
+
+  const structFollows = follows.map(item => ({
+    username: item.username,
+    profilePicUrl: item.profile_pic_url
+  }))
+
+  structFollows.forEach(user => {
+    const profile = structFollowers.find(follow => follow.username === user.username)
+
+    if (!profile) {
+      evidencias.push(user)
+    }
+  })
+
+  fs.writeFileSync('data.json', JSON.stringify(evidencias))
+  gerarPDFdata()
+}
+
+function gerarPDFdata() {
+  //fazer com o pipe para resolver arquivos muito grandes
+
+  const fileStream = fs.readFileSync('data.json')
+  const data = JSON.parse(fileStream)
+
+  fs.mkdirSync('images_insta')
+
+  const download = function (uri, filename, callback) {
+    request.head(uri, function (err, res, body) {
+      console.log('content-type:', res.headers['content-type']);
+      console.log('content-length:', res.headers['content-length']);
+
+      request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+    })
+  };
+
+  data.forEach(user => {
+    const filename = `${path.resolve(__dirname, '..', 'images_insta')}/${user.username}.jpg`
+    download(user.profilePicUrl, filename, () => console.log('downloaded', filename))
+  })
+
+  fs.rmSync('data.json')
+}
